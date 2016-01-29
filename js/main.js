d5adb41ffc31b774a628c7b4c3efbac8e59280ca
Samy Rabih <srabih@o2sources.com>
2016-01-29 19:00:58 +0100

    Suppression du callback sur le scroll

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 jQuery(function($) {'use strict';
-
-	// Navigation Scroll
-	$(window).scroll(function(event) {
-		//Scroll();
-	});
-
 	$('.navbar-collapse ul li a').on('click', function() {  
 		if ($(this.hash).length)
             $('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
