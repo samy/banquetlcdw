f85b81e66ae00d29a2ffd40f518fae2a66aee19f
Samy Rabih <srabih@o2sources.com>
2016-01-29 19:12:59 +0100

    Correction fonctionnement

--- js/main.js
+++ js/main.js
@@ -187,44 +187,43 @@ jQuery(function($) {'use strict';
 				$this.unbind('inview');
 			}
 		});
+        
+        // Contact form
+        var form = $('#main-contact-form');
+        form.submit(function(event){
+            event.preventDefault();
+            var form_status = $('<div class="form_status"></div>');
+            $.ajax({
+                url: $(this).attr('action'),
+                beforeSend: function(){
+                    form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+                }
+            }).done(function(data){
+                form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+            });
+        });
+        
+        //Pretty Photo
+        $("a[rel^='prettyPhoto']").prettyPhoto({
+            social_tools: false
+        });
+
+        //Google Map
+        var latitude = $('#google-map').data('latitude');
+        var longitude = $('#google-map').data('longitude');
+        function initialize_map() {
+            var myLatlng = new google.maps.LatLng(latitude,longitude);
+            var mapOptions = {
+                zoom: 14,
+                scrollwheel: false,
+                center: myLatlng
+            };
+            var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map
+            });
+        }
+        google.maps.event.addDomListener(window, 'load', initialize_map);
 	});
-
-	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-		});
-	});
-
-	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
-
-	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
-
 });
